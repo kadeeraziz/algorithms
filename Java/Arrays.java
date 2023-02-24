15231ad86bcb2843b428e5a8ba33b9c369edb32e
AZiz <q.aziz@campus.tu-berlin>
2023-02-24 19:32:46 +0100

    Declaring and Initializing  arrays

--- Java/Arrays.java
+++ Java/Arrays.java
@@ -1,5 +1,4 @@
 package Java;
-
 class Arrays{
 
     public static void main(String[] args) {
@@ -10,11 +9,22 @@ class Arrays{
             System.out.println(numbers[i]);
         }
 
-        System.out.println("Using for-each loop");
 
-        for(int i: numbers){
-            System.out.println(i);
+
+        // Add a new element to an array.
+        int new_element = 11;
+        int[] new_numbers = new int[numbers.length + 1];
+        for(int i=0; i < numbers.length; i++){
+            new_numbers[i] = numbers[i];
         }
+
+        new_numbers[new_numbers.length-1] = new_element;
+
+        for(int i=0; i < new_numbers.length; i++){
+            System.out.println(new_numbers[i]);
+        }
+
+     
     }
     
 
